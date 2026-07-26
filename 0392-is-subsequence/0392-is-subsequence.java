edfbad1c0576f1daffa3a7571b043ada536be855
Vaibhav verma <vaibhavverma6393@gmail.com>
2026-07-26 16:36:06 +0530

    Time: 5 ms (1.89%), Space: 43.1 MB (11.36%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = true;
+        }
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = false;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
